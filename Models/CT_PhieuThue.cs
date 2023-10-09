bb9616b2b13f0143757b8c132f8244dd2e86be3a
PhongTran <emphan1975@gmail.com>
2023-10-09 23:04:32 +0700

    Create git

---
+++ Models/CT_PhieuThue.cs
@@ -0,0 +1,10 @@
+namespace ProjectWeb.Models;
+public class CT_PhieuThue
+{
+    public string? MPT(get;set;);
+    public string? MAPhong(get;set;);
+    public string? NgayDen(get;set;);
+    public string? GioDen(get;set;);
+    public string? NgayDi(get;set;);
+    public string? NgayHD(get;set;);
+}
---
+++ Models/PhieuThueModel.cs
@@ -0,0 +1,7 @@
+namespace ProjectWeb.Models;
+public class PhieuThue
+{
+    public string? MPT(get;set;);
+    public string? MKH(get;set;);
+    public string? NgayLapPhieu(get;set;);
+}
