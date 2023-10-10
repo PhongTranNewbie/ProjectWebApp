b0dd14c145487a802b5a0c6185f3c6fc1996e347
Tuan Danh <nguyentuananh200601@gmail.com>
2023-10-10 16:17:43 +0700

    create 2 table client and type_client

---
+++ Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectWeb.Models
+{
+    public class Client
+    {
+        [Required,Range(10000000,999999999999),DisplayName("CCCD")]
+        public long Id { get; set; }
+        [Required,DisplayName("Họ")]
+        public string ho { get; set; }
+        [Required,DisplayName("TÊN")]
+        public string Name { get; set; }
+        [Required, DisplayName("SĐt"),Range(10000000000,999999999999)]
+        public long? phone { get; set; }
+        [ DisplayName("Email")]
+        public string? Email { get; set; }
+        [DisplayName("Địa chỉ")]
+        public string? place { get; set; }
+        public string? id_type_client { get; set; }
+
+
+    }
+}
---
+++ Models/Type_client.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectWeb.Models
+{
+    public class Type_client
+    {
+        public int Id_type_client { get; set; }
+        [Required,DisplayName("Loại khách")]
+        public string type_client { get; set; }
+    }
+}
