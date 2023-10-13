13e9efed2d7e8a1ae2815740c3eb024a6f145442
tuandanh123 <nguyentuananh200601@gmail.com>
2023-10-13 18:42:00 +0700

    luu

--- Models/CT_PhieuThueModels.cs
+++ Models/CT_PhieuThueModels.cs
@@ -1,10 +1,12 @@
-namespace ProjectWeb.Models;
-public class CT_PhieuThue
+namespace ProjectWeb.Models
 {
-    public string? MPT(get;set;);
-    public string? MAPhong(get;set;);
-    public string? NgayDen(get;set;);
-    public string? GioDen(get;set;);
-    public string? NgayDi(get;set;);
-    public string? NgayHD(get;set;);
+    public class CT_PhieuThue
+    {
+        public string? MPT { get; set; }
+        public string? MAPhong { get; set; }
+        public string? NgayDen { get; set; }
+        public string? GioDen { get; set; }
+        public string? NgayDi { get; set; }
+        public string? NgayHD { get; set; }
+    }
 }
