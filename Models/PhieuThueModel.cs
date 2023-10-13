2d79ff42f4d95b33dd518d4e8fd36c93ff2fdbcd
PhongTranNewbie <128611424+PhongTranNewbie@users.noreply.github.com>
2023-10-14 00:28:04 +0700

    Update PhieuThueModel.cs
    Revise

--- Models/PhieuThueModel.cs
+++ Models/PhieuThueModel.cs
@@ -4,9 +4,9 @@ namespace ProjectWeb.Models
 {
     public class PhieuThue
     {
-        [StringLength(5, ErrorMessage = "must have less than 5 characters.")]
+        [StringLength(5, ErrorMessage = "Must have less than 5 characters.")]
         public string MPT { get; set; }
-        [StringLength(5, ErrorMessage = "must have less than 5 characters.")]
+        [StringLength(5, ErrorMessage = "Must have less than 5 characters.")]
         public string MKH { get; set; }
         public DateTime NgayLapPhieu { get{return NgayLapPhieu; } set{ NgayLapPhieu = DateTime.Now;} }
     }
