03207035edc50a6e7f4928ca52f45773d83d4d94
TTNghia <tatrongnghiak15@siu.edu.vn>
2023-10-19 21:17:36 +0700

    Nghia Done

--- Models/CT_HoaDon.cs
+++ Models/CT_HoaDon.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectWeb.Models;
 public class CT_HoaDon
 {
+    [Required, StringLength(5, MinimumLength = 0, DisplayName("Mã Hóa Đơn")]
     public string MAHD { get; set; };
+    [Required, StringLength(5, MinimumLength = 0, DisplayName("Mã Phiếu Thuê")]
     public string MAPT { get; set; };
+    [Required, StringLength(5, MinimumLength = 0, DisplayName("Mã Phòng")]
     public string MAPHONG { get; set; };
-    public int SONGAY { get; set; };
+    [Range(1,100), DisplayName("Số ngày")]
+    public int? SONGAY { get; set; };
+    [Required, Range(500000, int.MaxValue), DisplayName("Đơn Giá")]
     public int DONGIA { get; set; };
+    [Required, Range(500000, int.MaxValue), DisplayName("Thành Tiền")]
     public int THANHTIEN { get; set; };
 }
